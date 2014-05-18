c5ae387df4c7431943da030976f84219c4523ecd
dustinstriplin <dustinstriplin@gmail.com>
2014-05-17 21:14:16 -0700

    2nd commit
    again

---
+++ src/Love.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class Love {
+	public static final boolean LOVE = true;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hey Jamie, Press a key");
+		Scanner scan = new Scanner(System.in);
+		scan.nextLine();
+		int i = 1;
+		while(LOVE) {
+			System.out.println("Jamie, I love you " + i + " times");
+			i++;
+		}
+	}
+
+}
